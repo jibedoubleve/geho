6b08e1b299370e7e8b8f0ca6ff1a4d69552bb3f9
jibedoubleve <jibedoubleve@gmail.com>
2015-11-21 14:34:52 +0100

    First commit
    Add default files

---
+++ Probel.Geho.Shell/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Probel.Geho.Shell
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        #region Methods
+
+        static void Main(string[] args)
+        {
+        }
+
+        #endregion Methods
+    }
+}
